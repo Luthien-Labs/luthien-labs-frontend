059c3bb77d14a079f0e444b7c61b37828120ab19
Josh Gibson <josh@joshuagibson.io>
2025-02-16 17:10:58 +0000

    feat: rejigged the import-export paradigm

---
+++ src/components/common/copyFrame/index.ts
@@ -0,0 +1,1 @@
+export { default } from "./CopyFrame.astro";
---
+++ src/components/common/index.ts
@@ -0,0 +1,3 @@
+export { default as ContentGrid } from "./contentGrid";
+export { default as CopyFrame } from "./copyFrame";
+export { default as ImageFrame } from "./imageFrame";
