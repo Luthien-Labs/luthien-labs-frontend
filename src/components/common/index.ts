08d84fcede7349e3b3aac619942b5712de170695
Josh Gibson <josh@joshuagibson.io>
2025-02-16 20:24:49 +0000

    feat: flex and grid alignment

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as ContentGrid } from "./contentGrid";
 export { default as CopyBlock } from "./copyBlock";
 export { default as GridItemCopy } from "./gridItemCopy";
 export { default as GridItemImage } from "./gridItemImage";
+export { default as Backdrop } from "./backdrop";
