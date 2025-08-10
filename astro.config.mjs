49a81f6182a57146b885a9673dbcf071e521c7a1
Josh Gibson <josh@joshuagibson.io>
2025-08-10 11:48:17 +0100

    feat: vite config x2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://www.luthien-labs.net",
+  server: {
+    host: "0.0.0.0",
+  },
 });
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 export default {
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['local.luthien-labs.net', 'luthien-labs.net'],
+  },
   preview: {
+    host: '0.0.0.0',
     allowedHosts: ['local.luthien-labs.net', 'luthien-labs.net'],
   },
 };
