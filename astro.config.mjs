0aa1f906001437884eb83e5cd5c21f045d5ae025
Josh Gibson <josh@joshuagibson.io>
2025-08-17 14:45:01 +0100

    chore: removed bad astro config line x2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
-import staticAdapter from "@astrojs/adapter-static";
 
 export default defineConfig({
   site: "https://www.luthien-labs.net",
   base: "/",
   output: "static",
-  adapter: "static",
   server: {
     host: "0.0.0.0",
   },
